51b2c0c9aa26355352d01accae53037fc4774dce
asyncs <ardasarpyenicesu@gmail.com>
2024-09-25 12:49:47 +0300

    revised inital commit

---
+++ src/HMAPBiman.h
@@ -0,0 +1,85 @@
+#ifndef __HMAPBIMAN_H
+#define __HMAPBIMAN_H
+
+#include <BotOp/bot.h>
+#include <Control/ShortPathMPC.h>
+#include <Control/timingOpt.h>
+#include <Optim/NLP_Solver.h>
+#include <KOMO/manipTools.h>
+#include <KOMO/pathTools.h>
+#include <KOMO/skeleton.h>
+#include <Kin/viewer.h>
+#include <Kin/F_forces.h>
+#include <Kin/forceExchange.h>
+#include <OptiTrack/optitrack.h>
+#include <chrono>
+#include <thread>
+#include <random>
+#include <PathAlgos/ConfigurationProblem.h>
+#include <PathAlgos/RRT_PathFinder.h>
+#include <Kin/kin.h>
+#include <Gui/opengl.h>
+#include <Geo/depth2PointCloud.h>
+#include <string>
+#include <Kin/cameraview.h>
+#include <Kin/frame.h>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+class HMAPBiman {
+public:
+    HMAPBiman(rai::Configuration C, rai::Configuration C2, arr qF, arr q_obs, std::string target, int total_obstacle_count, std::vector<std::string> tool_list, std::vector<std::string> gripper_list, double filter, std::string video_path, int waypoint_factor, bool view);
+    bool run();
+    arr getContactPoints();
+    arr getPath();
+private:
+    struct Quaternion {
+        double w, x, y, z;
+    };
+    rai::Configuration C;
+    rai::Configuration C2;
+    arr qHome;
+    arr qF;
+    arr q_obs;
+    arr path;
+    arr contact_points;
+    bool is_tool_aval;
+    bool is_aval_l;
+    bool is_aval_r;
+    bool is_feas;
+    bool is_path_aval;
+    bool is_path_blocked;
+    bool view;
+    int idx;
+    int obstacle_count;
+    int total_obstacle_count;
+    int img_count;
+    int fail_limit;
+    int waypoint_factor;
+    double calib;
+    double threshold;
+    double filter;
+    std::string tool;
+    std::string target;
+    std::string contact_point; 
+    std::string video_path;
+    std::vector<std::string> tool_list;
+    std::vector<std::string> gripper_list;
+    
+    rai::Frame& addMarker(rai::Configuration& C, const arr pos, const std::string& name, const std::string& parent, double size, bool is_relative, arr quat = {});
+    bool RRT(rai::Configuration& C2, arr& path, bool view = true, double rrt_extend_length = 0.04);
+    arr getCameraView(rai::Configuration& C, const std::string& cam_name, const std::string& target, const double filter = 0.5);
+    arr candidateContactPoint(rai::Configuration& C, const arr& pts, const int iter, bool isTransform = true);
+    const std::string generateContactPoint(rai::Configuration& C, const std::string& target, const std::string& waypoint, std::string& contact_point);
+    double calibSkeleton(rai::Configuration& C);
+    std::shared_ptr<SolverReturn> homeSkeleton(rai::Configuration& C, const std::string& l_gripper, const std::string& r_gripper, const std::string& l_gripper_home, const std::string& r_gripper_home);
+    std::shared_ptr<SolverReturn> moveSkeleton(rai::Configuration& C, const std::string& gripper, const std::string& target, const std::string& contact_point, const std::string& waypoint, const bool isTrial=false);
+    double toolSkeleton(rai::Configuration& C, const std::string& tool, const std::string& target, const std::string& gripper, const std::string& final_pose, const bool isTrial = false);
+    void toolSelection(rai::Configuration& C, const std::string waypoint, const std::string target, std::string& gripper_out, std::string& tool_out);
+    void homeTool(rai::Configuration& C, const std::string& gripper, const std::string& tool);
+    std::string useTool(rai::Configuration& C, const arr path_point, const std::string waypoint, const std::string target);
+};
+
+#endif
---
+++ test/bookshelf_task/main.cpp
@@ -0,0 +1,34 @@
+#include "HMAPBiman.h"
+
+/*----------------------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------MAIN------------------------------------------------------------*/
+/*----------------------------------------------------------------------------------------------------------------------*/
+int main(int argc, char* argv[]) {
+    rai::initCmdLine(argc, argv);
+    rnd.seed(0);
+
+    // Initialize Configuration and BotOp
+    rai::Configuration C;
+    C.addFile("../../src/config/bookshelf/HMAP_bookshelf_conf.g");  
+
+    rai::Configuration C2;
+    C2.addFile("../../src/config/bookshelf/HMAP_bookshelf_actuated_conf.g");  
+    
+    C.view(true, "Initial Configuration");
+
+    std::string target = "box";
+    std::vector<std::string> tool_list = {"stick"};
+    std::vector<std::string> gripper_list = {"r_l_gripper", "l_l_gripper"};
+    double filter = 0.01;
+    std::string video_path = "video/config";
+    int total_obstacle_count = 1;
+    int waypoint_factor = 2;
+    arr qF = {0.20, -0.57, 1.29, 1, 0, 0, 0};
+    arr q_obs = {-0.19, -0.4, 0.95, 1, 0, 0, 0};
+    C2.setJointState(C.getFrame("box")->getPose());
+    
+    HMAPBiman hmap_biman(C, C2, qF, q_obs, target, total_obstacle_count, tool_list, gripper_list, filter, video_path, waypoint_factor, true);
+    hmap_biman.run();
+
+    return 0;
+}
